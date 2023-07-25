f60757c286e87c85f3cd30b034d23c9d64cdefb3
CJay <chijiokennabueze@gmail.com>
2023-07-25 15:04:33 +0100

    working updated commit

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -57,8 +57,9 @@ def main():
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
     if page_selection == "Recommender System":
         # Header contents
-        st.write('# Movie Recommender Engine')
-        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
+        st.image('resources/imgs/name_image.png',use_column_width=True)
+        # st.write('# Movie Recommender Engine')
+        st.write('## ----------- Movie Recommender Engine -----------')
         st.image('resources/imgs/movie_page.png',use_column_width=True)
         # Recommender System algorithm selection
         sys = st.radio("Select an algorithm",
@@ -112,9 +113,41 @@ def main():
     # or to provide your business pitch.
 
     elif page_selection == "About Us":
-        st.title("FAQs for our movie recommender system app")
-        st.subheader("1. What is the purpose of this movie recommender system app?")
-        st.write("The app aims to provide personalized movie recommendations based on user preferences.")
+        st.title("About Us")
+        st.markdown(
+            """
+            __Welcome to our Movie Recommender App!__
+            
+            At RECORDmender, we believe that every movie night should be unforgettable.
+            We understand the excitement of discovering new films that captivate your heart
+            and transport you to different worlds. That's why we've built a powerful movie
+            recommendation engine at your fingertips.
+
+            Our app is designed to take the hassle out of choosing what to watch next.
+            With a vast database of movies spanning various genres, we ensure that there's
+            something for everyone. Whether you're in the mood for an adrenaline-pumping action
+            flick, a heartwarming romantic comedy, a mind-bending sci-fi adventure, or an
+            Oscar-worthy drama, we've got you covered.
+
+            But what sets us apart from the rest? Our recommendation algorithm is powered by
+            advanced machine learning and artificial intelligence techniques, constantly learning
+            and adapting to your unique movie preferences. As you rate movies, browse different
+            genres, and add films to your watchlist, our app intelligently tailors recommendations
+            specifically to your taste. Say goodbye to endless scrolling and indecisiveness
+            we're here to make your movie choices easier than ever before.
+
+            But that's not all. We also provide detailed movie information, including cast 
+            and crew, synopsis, ratings, and reviews, giving you all the insights you need to 
+            make an informed decision. Plus, our community of movie enthusiasts contributes their 
+            own ratings and reviews, making it a hub for lively discussions and recommendations. 
+
+            Whether you're a film buff, a casual viewer, or just looking for a great movie night 
+            with friends or family, our Movie Recommender App is your ultimate companion.
+            So sit back, relax, and let us take you on a cinematic journey like no other. 
+            Join our community today and let the movie magic unfold!"""
+        )
+        st.header("Meet The Team")
+        st.image('resources/imgs/team_bm2.png',use_column_width=True)
     
     elif page_selection == "Movie Search":
         api_key = "87d991ea"
@@ -136,8 +169,8 @@ def main():
             except:
                 st.error(f"No movie with title '{title}'")
     elif page_selection == "FAQ":
-        st.title("FAQs for our movie recommender")
-        faq_select = st.selectbox("Select below", list(faq.keys()))
+        st.title("Frequently Asked Questions")
+        faq_select = st.selectbox("Select from the dropdown below", list(faq.keys()))
         st.markdown(faq[faq_select])
         # st.selectbox("1. What is the purpose of this movie recommender system app?", faq_list.keys())
         # st.selectbox("1. What is the purpose of this movie recommender system app?", faq_list.keys())
--- utils/faq.py
+++ utils/faq.py
@@ -1,21 +1,21 @@
 faq = {
     "What is the purpose of this movie recommender system app?": 
-    "The app aims to provide personalized movie recommendations based on user preferences.",
+    "__The app aims to provide personalized movie recommendations based on user preferences.__",
 
     "How does the app suggest movies?": 
-    "The app uses a sophisticated algorithm that analyzes your watching history, ratings \
-    and preferences to suggest movies that you might enjoy.",
+    "__The app uses a sophisticated algorithm that analyzes your watching history, ratings \
+    and preferences to suggest movies that you might enjoy.__",
 
     "Are the recommendations always accurate?": 
-    "While our algorithm strives to provide accurate and relevant recommendations, movie \
+    "__While our algorithm strives to provide accurate and relevant recommendations, movie \
     preferences are subjective. You might not enjoy every suggested movie, but we constantly \
-    learn from your feedback to improve future recommendations.",
+    learn from your feedback to improve future recommendations.__",
 
     "Is the app available on multiple devices?": 
-    "Yes, the app is designed to be accessible across various devices, including smartphones, \
-    tablets, and computers. You can seamlessly switch between devices and continue where you left off.",
+    "__Yes, the app is designed to be accessible across various devices, including smartphones, \
+    tablets, and computers. You can seamlessly switch between devices and continue where you left off.__",
 
     "Can I provide feedback or report issues?": 
-    "Absolutely! We encourage users to provide feedback and report any issues they encounter. \
-    You can reach out to our support team through the app or our website.",
+    "__Absolutely! We encourage users to provide feedback and report any issues they encounter. \
+    You can reach out to our support team through the app.__",
 }
