ebb0788e9b5f034770587badb93ffd25ae5bf7e4
CJay <chijiokennabueze@gmail.com>
2023-07-27 12:21:24 +0100

    temp commit

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -121,11 +121,9 @@ def collab_model(movie_list,top_n=10):
 
     indices = pd.Series(movies_df['title'])
     movie_ids = pred_movies(movie_list)
-    df_init_users = pd.DataFrame(columns=ratings_df.columns)
+    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
     for i in movie_ids:
-        user_ratings_df = ratings_df[ratings_df['userId'] == i]
-        df_init_users = pd.concat([df_init_users, user_ratings_df], ignore_index=True)
-        
+        df_init_users=df_init_users._append(ratings_df[ratings_df['userId']==i])
     # Getting the cosine similarity matrix
     cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
     idx_1 = indices[indices == movie_list[0]].index[0]
